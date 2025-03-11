3fc4c8c9d4c242f361de5353b3a553a3ad9f24ee
Patrekur2007 <snorrasonpatrekurbjarni@gmail.com>
2025-03-11 10:38:39 +0000

    Add files via upload

---
+++ 1.2_PBS_lokið.py
@@ -0,0 +1,5 @@
+hæð = int(input("Sláðu inn hæð kassans í cm: "))#biður um tölu sem á að vera hæðinn 
+lengd = int(input("Sláðu inn lengd kassans í cm: "))#biður um tölu sem á að lengdinn
+breidd = int(input("Sláðu inn breidd kassans í cm: "))
+rúmmál = hæð * lengd * breidd
+print(f"Rúmmál kassans er: {rúmmál}")
